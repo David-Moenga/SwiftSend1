562257227e05c6d1ea38c286b0679df3419ffae9
David Moenga <moengadavid90@gmail.com>
2026-09-23 12:14:35 +0300

    Add PostgreSQL support for Render production

--- backend/swiftsend_api/settings.py
+++ backend/swiftsend_api/settings.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 from urllib.parse import urlsplit
 
+import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
 
 
@@ -84,12 +85,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "swiftsend_api.wsgi.application"
 ASGI_APPLICATION = "swiftsend_api.asgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {"default": dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
